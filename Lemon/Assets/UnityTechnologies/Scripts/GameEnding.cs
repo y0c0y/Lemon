d02fcacd560ec9fb7df0be3a6ed7ab2ffee88aa7
Cho Yunhee <68114427+y0c0y@users.noreply.github.com>
2024-06-07 23:44:13 +0900

    1.
    적 캐릭터 생성, 1부 : 정적 관찰자

---
+++ Lemon/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayAtExit = true;
+        }
+        
+    }
+
+    void Update()
+    {
+        if(m_IsPlayAtExit)
+        {
+            EndLevel();
+        }
+
+    }
+
+    void EndLevel ()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
